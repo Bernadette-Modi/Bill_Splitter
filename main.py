fe698398c7a63028d72a4b3ffe5ec7dbfb07d3b5
Bernadette Modi <berniemain353@gmail.com>
2025-03-29 01:19:35 +0300

    Completed calculation for person, tip, and total methods.

--- main.py
+++ main.py
@@ -6,4 +6,13 @@ class BillSplitter:
         self.num_people = num_people
         self.tip_calcualtion = tip_calculation
 
- 
+    def per_person(self) -> float:
+        self.total_bill / self.num_people
+
+    def tip_calculation(self) -> float:
+        self.tip_calcualtion / 100 * self.total_bill
+
+    def total_sum(self) -> float:
+        self.per_person() + self.tip_calcualtion()
+
+    
