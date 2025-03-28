73777a614db4816c9113e5a77c8561a9d0179d03
Bernadette Modi <berniemain353@gmail.com>
2025-03-29 01:04:19 +0300

    Initialized a class with a constructor.

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 print("\n Welcome to the bill splitter!")
+class BillSplitter:
+
+    def __init__(self, total_bill, num_people, tip_calculation):
+        self.total_bill = total_bill
+        self.num_people = num_people
+        self.tip_calcualtion = tip_calculation
+
+ 
