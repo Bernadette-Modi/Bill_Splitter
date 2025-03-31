20fbfe2c283ba4ffb0d9bc78a8a7ef23a5dea06c
Bernadette Modi <berniemain353@gmail.com>
2025-03-31 11:57:54 +0300

    Added the while loop.

--- main.py
+++ main.py
@@ -1,18 +1,40 @@
 print("\n Welcome to the bill splitter!")
 class BillSplitter:
 
-    def __init__(self, total_bill, num_people, tip_calculation):
+    def __init__(self, total_bill: float, num_people: int, tip_percentage: float):
         self.total_bill = total_bill
         self.num_people = num_people
-        self.tip_calcualtion = tip_calculation
+        self.tip_percentage = tip_percentage
 
     def per_person(self) -> float:
-        self.total_bill / self.num_people
+        return self.total_bill / self.num_people
 
     def tip_calculation(self) -> float:
-        self.tip_calcualtion / 100 * self.total_bill
+        return self.tip_percentage / 100 * self.total_bill
 
     def total_sum(self) -> float:
-        self.per_person() + self.tip_calcualtion()
+        if self.tip_percentage > 0:
+            return self.per_person() + self.tip_calculation()
+        return self.per_person()
 
-    
+while True:
+    total = float(input("Enter the total amount: ")) 
+    if total <= 0:
+        continue
+    break
+
+while True:
+    person = int(input("Enter number of people: "))
+    if person <= 0:
+        continue
+    break 
+
+while True:
+    tip = float(input("Enter the tip percentage: "))
+    if tip < 0:
+        continue
+    break
+
+bs = BillSplitter(total, person, tip)
+print(f"Each person will have to pay {bs.total_sum():.2f}.")
+
