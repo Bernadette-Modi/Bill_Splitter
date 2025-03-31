bf4517756e8e64ebcac3662971f286fa57c49efb
Bernadette Modi <berniemain353@gmail.com>
2025-03-31 12:14:12 +0300

    Added an error handling function and changed the while loop to make sure it does not crash.

--- main.py
+++ main.py
@@ -17,23 +17,20 @@ class BillSplitter:
             return self.per_person() + self.tip_calculation()
         return self.per_person()
 
-while True:
-    total = float(input("Enter the total amount: ")) 
-    if total <= 0:
-        continue
-    break
+def get_valid_input(prompt, type_func, condition):
+    while True:
+        try:
+            value = type_func(input(prompt))
+            if condition(value):
+                return value
+            else:
+                print("Invalid input. Please enter a valid value.")
+        except ValueError:
+            print("Invalid input. Please enter a number.")
 
-while True:
-    person = int(input("Enter number of people: "))
-    if person <= 0:
-        continue
-    break 
-
-while True:
-    tip = float(input("Enter the tip percentage: "))
-    if tip < 0:
-        continue
-    break
+total = get_valid_input("Enter the total amount: $", float, lambda x: x > 0)
+person = get_valid_input("Enter the number of people: ", int, lambda x: x > 0)
+tip = get_valid_input("Enter the tip percentage: ", float, lambda x: x >= 0)
 
 bs = BillSplitter(total, person, tip)
 print(f"Each person will have to pay {bs.total_sum():.2f}.")
